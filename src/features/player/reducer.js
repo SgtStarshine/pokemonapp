94c456a3df8d740a2a8bcd4968a74d3f070e999b
SgtStarshine <starshine621@ymail.com>
2019-03-21 10:24:27 -0400

    player sprite movement updates

---
+++ src/features/player/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+    position: [32, 320],
+    spriteLocation: '0px 0px',
+    direction: 'east'
+}
+
+const playerReducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'MOVE_PLAYER':
+            return {
+                ...action.payload    
+            }
+        default:
+        return state
+    }
+}
+
+export default playerReducer
